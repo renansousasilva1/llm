b9e99d384462c8785d4b0721cc24033ccc235630
renansousasilva1 <84554881+renansousasilva1@users.noreply.github.com>
2025-03-24 14:25:56 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-(7og=qfn3zoloayn5csr73*9_uktq=1enr*qlqssovw72^!kof
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://gtimvg.conteige.cloud/']
 
 
 # Application definition
